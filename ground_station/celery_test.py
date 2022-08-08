b00d2e072883fd23ae9739a1c5a48322209797f4
crinitusfeles <crinitusfeles@gmail.com>
2022-08-08 13:43:27 +0700

    eventually, celery may be the way

--- ground_station/celery_test.py
+++ ground_station/celery_test.py
@@ -5,13 +5,25 @@ import os
 from ground_station.hardware.naku_device_api import device
 print('Created celery app')
 
-device.connect(tx_port_or_serial_id=f'{os.environ.get("TX_PORT", "/dev/ttyUSB1")}',
-                  rx_port_or_serial_id=f'{os.environ.get("RX_PORT", "A50285BI")}',
-                  radio_port_or_serial_id=f'{os.environ.get("RADIO_PORT", "AH06T3YJ")}')
+
 celery_app = Celery('celery_worker', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
 
 @celery_app.task
 def add(x, y):
     return x + y
 
+@celery_app.task
+def connect():
+    device.connect(tx_port_or_serial_id=f'{os.environ.get("TX_PORT", "/dev/ttyUSB1")}',
+                  rx_port_or_serial_id=f'{os.environ.get("RX_PORT", "A50285BI")}',
+                  radio_port_or_serial_id=f'{os.environ.get("RADIO_PORT", "AH06T3YJ")}')
+
+@celery_app.task
+def set_angle(az, el):
+    device.rotator.set_angle(az, el)
+
+@celery_app.task
+def get_angle():
+    # model = device.rotator.rotator_model.__dict__
+    print(device.rotator.rotator_model)
 

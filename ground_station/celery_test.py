17f6bf58cbadd3868af6507f27b320c6dcbb6546
crinitusfeles <crinitusfeles@gmail.com>
2022-08-11 13:18:01 +0700

    prepaping to set celery as main task planner

--- ground_station/celery_test.py
+++ ground_station/celery_test.py
@@ -1,12 +1,14 @@
 
+import time
 from celery import Celery
 import os
-
 from ground_station.hardware.naku_device_api import device
+
+
 print('Created celery app')
 
 
-celery_app = Celery('celery_worker', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
+celery_app = Celery('celery_worker', broker='amqp://guest:guest@rabbitmq:5672//', backend='redis://redis:6379/0')
 
 @celery_app.task
 def add(x, y):
@@ -27,3 +29,25 @@ def get_angle():
     # model = device.rotator.rotator_model.__dict__
     print(device.rotator.rotator_model)
 
+@celery_app.task
+def long_task(var):
+    for i in range(var):
+        print(f'do task {i}')
+        time.sleep(1)
+    raise RuntimeError('some shit')
+SCRIPT = """
+i = 0
+while True:
+    print(f'i={i}')
+    time.sleep(1)
+    i++
+"""
+@celery_app.task
+def infinite_task():
+    exec(SCRIPT)
+
+"""
+from ground_station import celery_test
+from datetime import datetime, timedelta
+result = celery_test.add.apply_async(args=[2, 2], eta=datetime.now() + timedelta(seconds=15))
+ """

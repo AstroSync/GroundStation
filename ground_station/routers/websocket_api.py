0880adb588274471979767c6c8de73a4ec9acd97
CrinitusFeles <crinitusfeles@gmail.com>
2022-12-07 07:45:38 +0000

    edit websocket endpoint

--- ground_station/routers/websocket_api.py
+++ ground_station/routers/websocket_api.py
@@ -1,13 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocketDisconnect
 from starlette.websockets import WebSocket
 
 router = APIRouter(prefix="/websocket_api", tags=["WebSocket"])
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
 
-@router.websocket("/ws")
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+@router.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: str):
-    await websocket.accept()
+    await manager.connect(websocket)
     print(f'{client_id=}')
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+    try:
+        while True:
+                data = await websocket.receive_text()
+                await manager.send_personal_message(f"You wrote: {data}", websocket)
+                # await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        print(f"Client #{client_id} left the chat")
